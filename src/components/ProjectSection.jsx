64893aa458a3a5c0d7a8602ef5c1ed896a4de51a
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-07-14 19:28:24 +0530

    update path of img

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -23,7 +23,7 @@ const Projects = [
         id: 3,
         title: "Todo Reminder",
         description: "A platform for users to create and manage their todo lists. It allows users to add, edit, delete tasks, and set reminders.",
-        image: '/projects/todo-reminder.png',
+        image: '/projects/Todo-Reminder.png',
         tags: ['React', 'Tailwind CSS', 'Context API'],
         demoUrl: 'https://todo-context-api-pink.vercel.app/',
         githubUrl: 'https://github.com/bhavin0409/todoContextAPI'
