f2756584bf69d3553bb43606db34f59466b6c7b6
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-10-27 20:35:32 +0530

    remove theme toggle button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,7 @@ import ContactSection from '../components/ContactSection'
 const Home = () => {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
-        {/* Theme Toggle */}
-        <ThemeToggle/>
+        
 
         {/* Background Effects */}
         <StarBackground/>
