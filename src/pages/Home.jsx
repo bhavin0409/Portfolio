200c8880a19b9a102f7d03dd225f12e52431bf59
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-11-07 21:40:10 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import ThemeToggle from '../components/ThemeToggle'
+
 import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
